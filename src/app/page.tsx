6628e15ab328f45f82e7ee1f700c8976388cbc90
Евгений Сишненко <sishnenko86@mail.ru>
2023-09-19 19:39:47 +0300

    закончил настройки

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
+  const a = 4;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>ЗАДЕПЛОИЛ</h1>
+        <h1>ЗАДЕПЛОИЛ {a}</h1>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
